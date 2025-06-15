419b7a15e0fa14bf70904cc9546ce1405e4c2f6e
BrentLoaf <kcoldoowtnerbg@gmail.com>
2025-06-15 16:44:58 -0700

    Overhauled injury system (Now has levels from 1-5, each dealing a higher and higher slowness). And added 'Bleeding' when a player is bleeding, it will have a chance to damage the player every tick.

--- src/main/java/com/example/examplemod/BandageItem.java
+++ src/main/java/com/example/examplemod/BandageItem.java
@@ -1,6 +1,7 @@
 package com.example.examplemod;
 
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.player.Player;
@@ -8,23 +9,59 @@ import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
 
+import java.util.Map;
+
 public class BandageItem extends Item {
 
     public BandageItem(Properties p_41383_) {
         super(p_41383_);
     }
 
+    private static final Map<Integer, String> healMessages = Map.of(
+            0, "You feel fully healed",
+            1, "Your limp stops but you're still in pain",
+            2, "The ease of pain makes it easier to move",
+            3, "Staying up right is easier now, but you struggle to move still",
+            4, "You can move much easier than before, but the pain still aches"
+    );
+
+    private static final String stoppedBleeding = ", and the bleeding has stopped";
+
+    private static final String injuryKey = "hns_injury";
+    private static final String bleedingKey = "hns_bleeding";
+
     @Override
     public InteractionResultHolder<ItemStack> use(Level world, Player player, InteractionHand hand) {
         if (!world.isClientSide) {
             CompoundTag data = player.getPersistentData();
 
-            if (data.getBoolean("is_injured")) {
-                data.remove("is_injured");
+            int injuryIndex = data.getInt(injuryKey);
+            boolean isBleeding = data.getBoolean(bleedingKey);
 
+            boolean usedBandage = false;
+
+            String message = "You have no reason to use this bandage";
+
+            if (injuryIndex > 0) {
+                usedBandage = true;
+
+                data.putInt(injuryKey, --injuryIndex);
+                if (isBleeding) data.remove(bleedingKey);
+
+                message = healMessages.get(data.getInt(injuryKey)) + (isBleeding ? stoppedBleeding : "");
+            } else if (isBleeding) {
+                usedBandage = true;
+
+                data.remove(bleedingKey);
+                message = "The bleeding has stopped";
+            }
+
+            if (usedBandage) {
                 ItemStack stack = player.getItemInHand(hand);
                 stack.shrink(1);
             }
+
+            player.displayClientMessage(Component.nullToEmpty(message), true);
         }
 
         return InteractionResultHolder.success(player.getItemInHand(hand));
--- src/main/java/com/example/examplemod/DamageHandler.java
+++ src/main/java/com/example/examplemod/DamageHandler.java
@@ -1,6 +1,12 @@
 package com.example.examplemod;
 
+import net.minecraft.client.Minecraft;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.ChatType;
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.protocol.game.ClientboundSetActionBarTextPacket;
+import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.player.Player;
@@ -9,17 +15,62 @@ import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
+import java.util.Map;
+import java.util.Random;
+
+import static java.lang.String.format;
+
 @Mod.EventBusSubscriber(modid = ExampleMod.MODID)
 public class DamageHandler {
+
+    private static final Random random = new Random();
+
+    private static final Map<Integer, String> injuryMessages = Map.of(
+            1, "You feel a slight pain and slow slightly",
+            2, "You begin to limp. Pain is setting in",
+            3, "Your injury is getting worse. Moving and acting is harder",
+            4, "You're seriously hurt. You struggle to stay upright",
+            5, "You collapse from the pain. You can barely move"
+    );
+
+    private static final String bleedMessage = ", and have started bleeding";
+
+    private static final String injuryKey = "hns_injury";
+    private static final String bleedingKey = "hns_bleeding";
 
     @SubscribeEvent
     public static void onPlayerHurt(LivingHurtEvent event) {
         if (!(event.getEntity() instanceof Player player)) return;
         if (player.level.isClientSide) return;
 
+        if (event.getSource() == BleedingSource.BLEEDING) return;
+
         CompoundTag data = player.getPersistentData();
-        if (!data.getBoolean("is_injured")) {
-            data.putBoolean("is_injured", true);
+        boolean changed = false;
+        boolean startedBleeding = false;
+
+        if (random.nextInt(3) == 0) {
+            int injuryIndex = data.getInt(injuryKey);
+
+            if (injuryIndex >= 5) return;
+
+            data.putInt(injuryKey, ++injuryIndex);
+
+            if (random.nextInt(3) == 0) {
+                boolean isBleeding = data.getBoolean(bleedingKey);
+
+                if (!isBleeding) {
+                    data.putBoolean(bleedingKey, true);
+                    startedBleeding = true;
+                }
+            }
+
+            changed = true;
+        }
+
+        if (changed) {
+            String message = startedBleeding ? injuryMessages.get(data.getInt(injuryKey)) + bleedMessage : injuryMessages.get(data.getInt(injuryKey));
+            player.displayClientMessage(Component.nullToEmpty(message), true);
         }
     }
 
@@ -28,8 +79,24 @@ public class DamageHandler {
         Player player = event.player;
 
         CompoundTag data = player.getPersistentData();
-        if (data.getBoolean("is_injured")) {
-            player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 20, 0, true, false));
+
+        int injuryIndex = data.getInt(injuryKey);
+        boolean isBleeding = data.getBoolean(bleedingKey);
+
+        if (injuryIndex > 0) {
+            player.addEffect(new MobEffectInstance(MobEffects.MOVEMENT_SLOWDOWN, 1, --injuryIndex, true, false));
+
+            if (isBleeding) {
+                int randomVal = random.nextInt(50);
+
+                if (randomVal <= injuryIndex * 5) {
+                    player.hurt(BleedingSource.BLEEDING, ((float) injuryIndex * 0.5F));
+                }
+            }
         }
+    }
+
+    public class BleedingSource {
+        public static final DamageSource BLEEDING = new DamageSource("bleeding").bypassArmor();
     }
 }
