20ae4572e0d0796d97be78d41b97101a933b3810
BrentLoaf <kcoldoowtnerbg@gmail.com>
2025-06-15 02:37:47 -0700

    First mod version. Added bandage item to misc section of the creative tab.

---
+++ src/main/java/com/example/examplemod/ModItems.java
@@ -0,0 +1,14 @@
+package com.example.examplemod;
+
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ExampleMod.MODID);
+
+    public static final RegistryObject<Item> BANDAGE =
+            ITEMS.register("bandage", () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MISC)));
+}
